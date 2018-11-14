7b95a0a6a6f99fafc4971fedb3f0ab9f040aa72a
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2018-11-14 23:46:32 +0000

    Room Prefabs changed

--- Uni Project/Unnamed Game/Assets/Scripts/Dungeon Scripts/roomTemplates.cs
+++ Uni Project/Unnamed Game/Assets/Scripts/Dungeon Scripts/roomTemplates.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class roomTemplates : MonoBehaviour {
 
@@ -8,6 +9,7 @@ public class roomTemplates : MonoBehaviour {
 	public GameObject[] topRooms;
 	public GameObject[] leftRooms;
 	public GameObject[] rightRooms;
+	public GameObject[] puzzlesBoiii;
 
 	public GameObject closedRoom;
 
@@ -19,8 +21,17 @@ public class roomTemplates : MonoBehaviour {
 
 	private destroyer Destroyer;
 
+	private GameObject puzzleRoom;
+	public bool puzzleSpawned = false;
+	public bool helpSpawned;
+	private string[] puzzleDoors;
+
 	void Start(){
 		Destroyer = GameObject.FindGameObjectWithTag ("Entry").GetComponentInChildren<destroyer> ();
+		// hardcoding values for the only puzzle room. will be a switch once more exist
+		helpSpawned = false;
+		puzzleDoors [0] = "T";
+		puzzleDoors [1] = "B";
 	}
 
 	void Update(){
@@ -28,8 +39,26 @@ public class roomTemplates : MonoBehaviour {
 			spawnedBoss = true;
 			Destroyer.destroyself();
 			Instantiate (boss, rooms[rooms.Count-1].transform.position, Quaternion.identity);
+			if (rooms.Count >= 8) {
+				spawnPuzzle ();
+			}
 		} else {
 			waitTime -= Time.deltaTime;
 		}
+	}
+
+	void spawnPuzzle(){
+		bool canSpawnPuzzle = true;
+		puzzleRoom = rooms[Random.Range (5, rooms.Count - 2)];
+		foreach (Transform child in puzzleRoom.transform){
+			Debug.Log (child.CompareTag ("spawnPoint"));
+			if (child.CompareTag("spawnPoint")){
+				int openingDir = child.GetComponent<roomSpawner> ().openingDirection;
+				if (openingDir == 3 || openingDir == 4){
+					canSpawnPuzzle = false;
+				}
+			}
+		}
+		Debug.Log (canSpawnPuzzle + " " + puzzleRoom.name);
 	}
 }

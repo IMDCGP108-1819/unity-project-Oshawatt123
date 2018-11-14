a944c5a846b78b70715361724e537d57ed554513
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2018-11-14 19:10:33 +0000

    BitWise Research
    Researched into BitWise operations and what the LayerMask actually stands for and what happens with all the bitshifting.
    Attempted to make a different method for differentiating between the layers but this method works fine for now. No need to update until any issues occur surrounding different enemies

--- Uni Project/Unnamed Game/Assets/Scripts/playerAttack.cs
+++ Uni Project/Unnamed Game/Assets/Scripts/playerAttack.cs
@@ -52,12 +52,12 @@ public class playerAttack : MonoBehaviour {
 				notAlreadyAttacked = false;
 				Collider2D[] enemiesToDamage = Physics2D.OverlapBoxAll (attackPos.position, new Vector2 (attackRangeX, attackRangeY), 0, mobLayer);
 				for (int i = 0; i < enemiesToDamage.Length; i++) {
-					Debug.Log ((1 << playerLayer) & mobLayer);
+					//Debug.Log ((1 << playerLayer) & mobLayer);
 					if (((1 << enemyLayer) & mobLayer) != 0){
-						Debug.Log ("Hitting enemy");
+						//Debug.Log ("Hitting enemy");
 						enemiesToDamage [i].GetComponentInParent<Enemy_Basic> ().takeDamage (damage);
 					} else if (((1 << playerLayer) & mobLayer) != 0) {
-						Debug.Log ("Try enemy");
+						//Debug.Log ("Try enemy");
 						enemiesToDamage [i].GetComponentInParent<PlayerMovement>().takeDamage (damage);
 					}
 				}
